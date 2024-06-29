58a4339bd4f08eec639047dac3d0bb8a301a898e
Сергій Гололобов <gololobov.serhiy@gmail.com>
2024-06-29 19:15:35 +0300

    29.06.2024

---
+++ 7_NetworkApp_WebClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+
+namespace _7_NetworkApp_WebClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSK5-EV8oc2SaUhoDOq3Bxbuk2bq10ILhuiGA&s";
+            //string localPath = "logo.png";
+            //try
+            //{
+            //    using (WebClient client = new WebClient())
+            //    {
+            //        client.DownloadFile(url, localPath);
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    throw;
+            //}
+
+
+
+            //string url = "https://www.itstep.org";
+            //try
+            //{
+            //    using (WebClient client = new WebClient())
+            //    {
+            //        string htmlCode = client.DownloadString(url);
+            //        Console.WriteLine(htmlCode);
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    throw;
+            //}
+
+
+            //string url = "https://www.youtube.com/watch?v=GhQdlIFylQ8";
+            //try
+            //{
+            //    using (WebClient client = new WebClient())
+            //    {
+            //        client.DownloadFile(url, "vid.mp4");
+          
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    throw;
+            //}
+           
+
+
+        }
+    }
+}
